cb7bf99831f54e9520301482235bf217cb887de6
iago fagundes <iago.fagundes@outlook.com>
2021-10-06 14:01:00 -0300

    feat: pizza entity

---
+++ src/modules/domain/entities/Pizza.ts
@@ -0,0 +1,14 @@
+import { Column, Entity, Index, PrimaryColumn } from 'typeorm'
+
+@Index('PK__TamanhoP__AB3F3DE5005FFE8A', ['pizzaCode'], { unique: true })
+@Entity('TamanhoPizza', { schema: process.env.NODE_ENV !== 'production' ? 'public' : 'dbo' })
+export class Pizza {
+  @PrimaryColumn({ type: 'int', name: 'cdTamanhoPizza' })
+  pizzaCode!: number;
+
+  @Column({ type: 'int', name: 'nrFatiaMaxQtd', nullable: true })
+  maxSlices!: number;
+
+  @Column({ type: 'varchar', name: 'dsDescricao', nullable: true, length: 50 })
+  description?: string;
+}
---
+++ src/modules/domain/entities/PizzaFlavors.ts
@@ -0,0 +1,14 @@
+import { Column, Entity, Index, PrimaryColumn } from 'typeorm'
+
+@Index('PK_IteTamanhoPizza', ['pizzaCode', 'productCode'], { unique: true })
+@Entity('IteTamanhoPizza', { schema: process.env.NODE_ENV !== 'production' ? 'public' : 'dbo' })
+export class PizzaFlavors {
+  @PrimaryColumn({ type: 'int', name: 'cdTamanhoPizza' })
+  pizzaCode!: number;
+
+  @PrimaryColumn({ type: 'int', name: 'cdProduto' })
+  productCode!: number;
+
+  @Column('decimal', { name: 'vlTamanhoPreco', nullable: true, scale: 2 })
+  value!: number;
+}
